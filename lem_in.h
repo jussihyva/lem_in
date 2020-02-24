80beba3c40c81e06343923dd8b62ac295bf06863
Juhani Kauppi <jkauppi@student.hive.fi>
2020-02-24 11:26:00 +0200

    ft_printf leak issue fixed (Maybe). Leaks in case of %s and param value is "" string (empty).

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/23 13:51:44 by jkauppi           #+#    #+#             */
-/*   Updated: 2020/02/23 17:46:34 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/02/24 10:12:52 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,44 +21,44 @@ typedef enum	e_opt
 	leaks = 0x1
 }				t_opt;
 
-typedef struct		s_move_line
-{
-	s_move_action	move_action;
-}			t_move_line;
-
 typedef struct		s_room
 {
 	char	*name;
-	int	coord_x;
-	int	coord_y;
-	int	id;
-}			t_roon;
+	int		coord_x;
+	int		coord_y;
+	int		id;
+}					t_room;
 
 typedef struct		s_history
 {
 	size_t		line_nr;
 	t_room		room;
 	size_t		move_order;
-}			t_history;
+}					t_history;
 
 typedef struct		s_ant
 {
 	int		id;
 	t_room		current_room;
 	t_history	*room_history;
-}			t_ant;
+}					t_ant;
 
-typedef struct		t_move_action
+typedef struct		s_move_action
 {
-	s_ant		ant;
-	s_room		room;
-}
+	t_ant		ant;
+	t_room		room;
+}					t_move_action;
+
+typedef struct		s_move_line
+{
+	t_move_action	move_action;
+}					t_move_line;
 
 typedef struct		s_transportation
 {
 	t_ant		*ants;
 	size_t		number_of_ants;
-	int128		*room_reservation;
+	int64_t		*room_reservation;
 	t_list		*move_lines;
 }			t_transportation;
 
