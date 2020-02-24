d7310cea0068ea682c4655081dd6ca663df0cc03
Juhani Kauppi <57214156+jussihyva@users.noreply.github.com>
2020-02-24 03:31:56 +0200

    Structure for room and ant are created.

--- lem_in.h
+++ lem_in.h
@@ -21,6 +21,28 @@ typedef enum	e_opt
 	leaks = 0x1
 }				t_opt;
 
+typedef struct		s_room
+{
+	char	*name;
+	int	coord_x;
+	int	coord_y;
+	int	id;
+}			t_roon;
+
+typedef struct		s_history
+{
+	size_t		line_nr;
+	t_room		room;
+	size_t		move_order;
+}			t_history;
+
+typedef struct		s_ant
+{
+	int		id;
+	t_room		current_room;
+	t_history	*room_history;
+}			t_ant;
+
 t_opt			ft_read_opt(int *argc, char ***argv);
 
 #endif
