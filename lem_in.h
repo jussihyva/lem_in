fc1e914df9c013afd0ad3e77f611120708505449
Juhani Kauppi <57214156+jussihyva@users.noreply.github.com>
2020-02-24 08:42:58 +0200

    Transportation and move action structures are created.

--- lem_in.h
+++ lem_in.h
@@ -21,6 +21,11 @@ typedef enum	e_opt
 	leaks = 0x1
 }				t_opt;
 
+typedef struct		s_move_line
+{
+	s_move_action	move_action;
+}			t_move_line;
+
 typedef struct		s_room
 {
 	char	*name;
@@ -42,6 +47,19 @@ typedef struct		s_ant
 	t_room		current_room;
 	t_history	*room_history;
 }			t_ant;
+
+typedef struct		t_move_action
+{
+	s_ant		ant;
+	s_room		room;
+}
+
+typedef struct		s_transportation
+{
+	t_ant		*ants;
+	int128		*room_reservation;
+	t_list		*move_lines;
+}			t_transportation;
 
 t_opt			ft_read_opt(int *argc, char ***argv);
 
