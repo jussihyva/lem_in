afc515570221cfac4d3891c561bbb60d963eb6ea
Juhani Kauppi <57214156+jussihyva@users.noreply.github.com>
2020-02-29 11:36:10 +0200

    invalid_room_name Implemented.

--- src/manage_room_data.c
+++ src/manage_room_data.c
@@ -63,6 +63,8 @@ static t_read_status	validate_room_data(char *line, t_input *input,
 		c++;
 	if (c == 3)
 	{
+		if (splitted_line[0] == 'L')
+			input->error = invalid_room_name;
 		room_ptr = add_room(splitted_line, input);
 		if (read_status == read_start_room_data)
 			input->start_room_ptr = room_ptr;
