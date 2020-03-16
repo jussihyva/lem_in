806685aaf04d001b2a40291ebb3175d26b33dad5
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-03-16 14:43:06 +0200

    search two best paths.

--- src/select_paths.c
+++ src/select_paths.c
@@ -12,15 +12,15 @@
 
 #include "lem_in.h"
 
-static void				update_best_room(t_room *next_room, t_room *best_room,
+static void				update_best_room(t_room **next_room, t_room **best_room,
 														t_validity *validity)
 {
-	if (next_room->num_of_conn_to_end < best_room->num_of_conn_to_end)
+	if ((*next_room)->num_of_conn_to_end < (*best_room)->num_of_conn_to_end)
 	{
-		best_room = next_room;
+		*best_room = *next_room;
 		*validity = valid_room;
 	}
-	else if (next_room->num_of_conn_to_end == best_room->num_of_conn_to_end)
+	else if ((*next_room)->num_of_conn_to_end == (*best_room)->num_of_conn_to_end)
 		*validity = many_alternatives;
 	return ;
 }
@@ -33,14 +33,14 @@ static t_room			*get_next_best_room(t_list *adj_room_elem,
 
 	best_room = NULL;
 	*validity = no_room;
-	while (adj_room_elem && *validity != many_alternatives)
+	while (adj_room_elem)
 	{
 		next_room = *(t_room **)adj_room_elem->content;
 		if ((next_room == end_room_ptr) || (next_room->num_of_conn_to_end &&
 														!next_room->is_visited))
 		{
 			if (best_room)
-				update_best_room(next_room, best_room, validity);
+				update_best_room(&next_room, &best_room, validity);
 			else
 			{
 				best_room = next_room;
@@ -53,7 +53,7 @@ static t_room			*get_next_best_room(t_list *adj_room_elem,
 }
 
 static t_validity		create_path(t_input *input, t_list **path,
-														t_list *adj_room_elem)
+											t_list *adj_room_elem, int forced)
 {
 	int				end_room_reached;
 	t_room			*best_room;
@@ -65,8 +65,10 @@ static t_validity		create_path(t_input *input, t_list **path,
 		validity = no_room;
 		best_room = get_next_best_room(adj_room_elem, input->end_room_ptr,
 																&validity);
-		if (validity == valid_room)
+		if ((validity == valid_room) ||
+									(!forced && validity == many_alternatives))
 		{
+			validity = valid_room;
 			best_room->is_visited = 1;
 			ft_lstadd_e(path, ft_lstnew(&best_room, sizeof(best_room)));
 			if (best_room == input->end_room_ptr)
@@ -80,7 +82,7 @@ static t_validity		create_path(t_input *input, t_list **path,
 	return (validity);
 }
 
-static t_valid_path		*find_shortest_path(t_input *input)
+static t_valid_path		*find_shortest_path(t_input *input, int forced)
 {
 	t_valid_path	*valid_path;
 	t_list			**path;
@@ -91,7 +93,7 @@ static t_valid_path		*find_shortest_path(t_input *input)
 	ft_lstadd_e(path, ft_lstnew(&input->start_room_ptr,
 											sizeof(input->start_room_ptr)));
 	adj_room_elem = input->start_room_ptr->connection_lst;
-	validity = create_path(input, path, adj_room_elem);
+	validity = create_path(input, path, adj_room_elem, forced);
 	if (validity != no_room)
 	{
 		valid_path = (t_valid_path *)ft_memalloc(sizeof(*valid_path));
@@ -133,17 +135,36 @@ int						select_paths(t_input *input, t_report *report)
 {
 	t_valid_path	*valid_path;
 	size_t			num_of_paths;
+	t_list			*elem;
+	t_list			*path_elem;
+	t_list			*adj_room_elem;
+	t_validity		validity;
 
-	num_of_paths = 2;
+	num_of_paths = 3;
 	report->number_of_paths = 0;
-	valid_path = find_shortest_path(input);
-	while (valid_path && report->number_of_paths < num_of_paths)
+	valid_path = find_shortest_path(input, num_of_paths - 1);
+	while (valid_path && num_of_paths)
 	{
 		ft_lstadd(report->lst_of_valid_paths, ft_lstnew(&valid_path,
 														sizeof(valid_path)));
 		report->number_of_paths++;
-		if (report->number_of_paths < num_of_paths)
-			valid_path = find_shortest_path(input);
+		num_of_paths--;
+		if (num_of_paths)
+			valid_path = find_shortest_path(input, num_of_paths);
+	}
+	elem = *(t_list **)report->lst_of_valid_paths;
+	while (elem)
+	{
+		valid_path = *(t_valid_path **)elem->content;
+		if (valid_path->validity == many_alternatives)
+		{
+			path_elem = *valid_path->path;
+			while (path_elem->next)
+				path_elem = path_elem->next;
+			adj_room_elem = (*(t_room **)path_elem->content)->connection_lst;
+			validity = create_path(input, valid_path->path, adj_room_elem, 0);
+		}
+		elem = elem->next;
 	}
 	if (report->number_of_paths)
 		put_ants_to_paths(report);
