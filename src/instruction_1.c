f462de661fa7c8cae5da4817b43c0ca2c1003c97
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-03-26 09:23:49 +0200

    Checker implementation.

--- src/instruction_1.c
+++ src/instruction_1.c
@@ -6,11 +6,37 @@
 /*   By: pi <pi@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/22 18:32:40 by pi                #+#    #+#             */
-/*   Updated: 2020/03/24 15:32:09 by pi               ###   ########.fr       */
+/*   Updated: 2020/03/26 09:04:58 by pi               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+void					print_instruction(t_input *input)
+{
+	t_list					*instruction_line_elem;
+	t_list					*elem;
+	t_instruction_line		*instruction_line;
+	char					*ant_name;
+	char					*room_name;
+
+	instruction_line_elem = *input->instruction_line_lst;
+	while (instruction_line_elem)
+	{
+		instruction_line = (t_instruction_line *)instruction_line_elem->content;
+		elem = *instruction_line->instruction_lst;
+		while (elem)
+		{
+			ant_name = ((t_instruction *)elem->content)->ant->name;
+			room_name = ((t_instruction *)elem->content)->room->name;
+			ft_printf("%5s-%-3s", ant_name, room_name);
+			elem = elem->next;
+		}
+		ft_printf("\n");
+		instruction_line_elem = instruction_line_elem->next;
+	}
+	return ;
+}
 
 static int				add_instruction(char *instruction_string,
 									t_input *input, t_list **instruction_lst)
@@ -44,21 +70,19 @@ static void				split_instruction_line(char *line, t_input *input)
 	char					**instruction_string_array;
 	size_t					c;
 	int						result;
-	t_instruction_line		*instruction_line;
+	t_instruction_line		instruction_line;
 
-	instruction_line = (t_instruction_line *)ft_memalloc(sizeof(*instruction_line));
-	instruction_line->instruction_lst = (t_list **)ft_memalloc(sizeof(*instruction_line->instruction_lst));
-//	instruction_lst = NULL;
+	instruction_line.instruction_lst = (t_list **)ft_memalloc(sizeof(*instruction_line.instruction_lst));
 	instruction_string_array = ft_strsplit(line, ' ');
 	result = 1;
 	c = -1;
 	while (instruction_string_array[++c] && result)
 		result = add_instruction(instruction_string_array[c], input,
-											instruction_line->instruction_lst);
-	if (*instruction_line->instruction_lst)
+											instruction_line.instruction_lst);
+	if (*instruction_line.instruction_lst)
 	{
-		elem = ft_lstnew(instruction_line, sizeof(*instruction_line));
-		ft_lstadd(input->instruction_line_lst, elem);
+		elem = ft_lstnew(&instruction_line, sizeof(instruction_line));
+		ft_lstadd_e(input->instruction_line_lst, elem);
 	}
 	ft_arraydel(instruction_string_array);
 	return ;
@@ -67,10 +91,6 @@ static void				split_instruction_line(char *line, t_input *input)
 void					read_instruction_data(char *line, t_input *input)
 {
 	t_list					*elem;
-	t_list					*instruction_line_elem;
-	t_instruction_line		*instruction_line;
-	char					*ant_name;
-	char					*room_name;
 
 	if (line[0] == '#')
 		;
@@ -79,21 +99,6 @@ void					read_instruction_data(char *line, t_input *input)
 		elem = ft_lstnew(line, sizeof(*line) * (ft_strlen(line) + 1));
 		ft_lstadd(input->valid_input_lines, elem);
 		split_instruction_line(line, input);
-		instruction_line_elem = *input->instruction_line_lst;
-		if (instruction_line_elem)
-		{
-			instruction_line = (t_instruction_line *)instruction_line_elem->content;
-			elem = *instruction_line->instruction_lst;
-			while (elem)
-			{
-				ant_name = ((t_instruction *)elem->content)->ant->name;
-				room_name = ((t_instruction *)elem->content)->room->name;
-				ft_printf("%5s-%-3s", ant_name, room_name);
-				elem = elem->next;
-			}
-			ft_printf("\n");
-			instruction_line_elem = instruction_line_elem->next;
-		}
 	}
 	return ;
 }
