f4507c3fc3ff5a36a3f6af943faeda4c397646ad
Ubuntu <ubuntu@ip-172-31-87-195.ec2.internal>
2020-05-04 12:34:07 +0300

    lem_in and checker ready for release

--- src/input_2.c
+++ src/input_2.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/29 05:30:49 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/05/04 12:23:16 by ubuntu           ###   ########.fr       */
+/*   Updated: 2020/05/04 12:29:37 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,9 @@ void				select_algorithms(t_list **algorithm_lst)
 	t_list			*elem;
 
 	*algorithm_lst = NULL;
-	// algorithm.function = (void *)select_paths;
-	// elem = ft_lstnew(&algorithm, sizeof(&algorithm));
-	// ft_lstadd(algorithm_lst, elem);
-	// algorithm.function = (void *)algorithm_ford_fulkerson1;
-	// elem = ft_lstnew(&algorithm, sizeof(algorithm));
-	// ft_lstadd(algorithm_lst, elem);
-	// algorithm.function = (void *)algorithm_ford_fulkerson2;
-	// elem = ft_lstnew(&algorithm, sizeof(algorithm));
-	// ft_lstadd(algorithm_lst, elem);
 	algorithm.function = (void *)algorithm_ford_fulkerson3;
 	elem = ft_lstnew(&algorithm, sizeof(algorithm));
 	ft_lstadd(algorithm_lst, elem);
-	// algorithm.function = (void *)algorithm_ford_fulkerson4;
-	// elem = ft_lstnew(&algorithm, sizeof(algorithm));
-	// ft_lstadd(algorithm_lst, elem);
-	// algorithm.function = (void *)algorithm_ford_fulkerson5;
-	// elem = ft_lstnew(&algorithm, sizeof(algorithm));
-	// ft_lstadd(algorithm_lst, elem);
 	algorithm.function = (void *)algorithm_ford_fulkerson6;
 	elem = ft_lstnew(&algorithm, sizeof(algorithm));
 	ft_lstadd(algorithm_lst, elem);
