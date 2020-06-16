a1336fd617db0fbdc2144c3400279dec95650c3f
Ubuntu <ubuntu@ip-172-31-87-195.ec2.internal>
2020-06-16 03:39:16 +0300

    leak issue fixed.

--- src/room_data_1.c
+++ src/room_data_1.c
@@ -3,18 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   room_data_1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 15:50:13 by jkauppi           #+#    #+#             */
-/*   Updated: 2020/06/10 20:21:07 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/06/11 09:40:25 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+static char				*create_mod_line(t_room *room)
+{
+	char			*mod_line;
+	char			*s;
+
+	mod_line = (char *)ft_memalloc(sizeof(*mod_line) * 10000);
+	ft_strcat(mod_line, room->name);
+	ft_strcat(mod_line, " ");
+	s = ft_itoa(room->coord_x);
+	ft_strcat(mod_line, s);
+	ft_strdel(&s);
+	ft_strcat(mod_line, " ");
+	s = ft_itoa(room->coord_y);
+	ft_strcat(mod_line, s);
+	ft_strdel(&s);
+	return (mod_line);
+}
+
 static void				add_input_line(t_input *input, char *line, int add_line)
 {
 	t_list			*elem;
+	t_room			*room;
 	char			*mod_line;
 
 	if (add_line)
@@ -26,12 +45,8 @@ static void				add_input_line(t_input *input, char *line, int add_line)
 		}
 		else
 		{
-			mod_line = (char *)ft_memalloc(sizeof(*mod_line) * 10000);
-			ft_strcat(mod_line, (*((t_room **)input->room_lst->content))->name);
-			ft_strcat(mod_line, " ");
-			ft_strcat(mod_line, ft_itoa((*((t_room **)input->room_lst->content))->coord_x));
-			ft_strcat(mod_line, " ");
-			ft_strcat(mod_line, ft_itoa((*((t_room **)input->room_lst->content))->coord_y));
+			room = *(t_room **)input->room_lst->content;
+			mod_line = create_mod_line(room);
 			elem = ft_lstnew(mod_line, sizeof(*mod_line) *
 													(ft_strlen(mod_line) + 1));
 			ft_lstadd(input->valid_input_lines, elem);
